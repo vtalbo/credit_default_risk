7e2b65990ae10abe288606924ca52c144e6169cc
vtalbo <vincent.talbo@gmail.com>
2021-04-03 21:13:00 +0200

    improve Procfile

---
+++ app/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/dashboard/')
+def dashboard():
+    return "Bienvenue sur le <i>dashboard</i>"
+
