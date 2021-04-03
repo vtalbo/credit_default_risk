1ca1d1bf2ecc2b9ef583732e919b4cc32022ab97
vtalbo <vincent.talbo@gmail.com>
2021-04-03 21:21:36 +0200

    commit-test-add gitignore

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
 @app.route('/dashboard/')
 def dashboard():
     return "Bienvenue sur le <i>dashboard</i>"
-
