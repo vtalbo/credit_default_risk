c5e8486985b5479fb02b658677ad2c66b3a1cd94
vtalbo <vincent.talbo@gmail.com>
2021-06-13 19:23:25 +0200

    lightgbm requirement fixed

--- app/functions/__init__.py
+++ app/functions/__init__.py
@@ -8,7 +8,7 @@ def predict_credit(credit_id):
     """From credit_id, get the prediction of the credit failure or not
     If the credit_id does not exist, an error is shown"""
     try:
-        test = pd.read_csv('m_test_10.csv', index_col='SK_ID_CURR')
+        test = pd.read_csv('m_test.csv', index_col='SK_ID_CURR')
         x = test.loc[int(credit_id), :]
         # load the model from disk
         with open('finalized_model.sav', 'rb') as file:
